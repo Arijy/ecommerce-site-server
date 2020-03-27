ccf0e2c1491874efaf1c671d7bda4dea60f3307b
Arijy <arijchaudhary@gmail.com>
2020-03-27 17:16:39 +0500

    my-commit

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-let express=require('express')
-let app=express()
-let bodyParser = require ('body-parser')
-let cors = require('cors')
-let data = require('./data')
-let port= 4000
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
-
-app.get('/',(req,res) =>
-res.send('Hello world'))
-app.get('/animals',(req,res) =>
-res.json(data.animals)
-)
-
-app.listen(port)
-console.log('api running on'+ port + ':')
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+let cors = require("cors");
+let data = require("./data");
+let port = 4000;
+ 
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.get("/", (req, res) => res.send("Hello world"));
+app.get("/products", (req, res) => res.json(data.products));
+app.listen(port);
+ 
+console.log("api running on" + port + ":");
