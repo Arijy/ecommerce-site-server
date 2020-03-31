e6687491a2fda09f7e71d89b4efdeb6038528305
Arijy <arijchaudhary@gmail.com>
2020-03-31 21:02:28 +0500

    my commit

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.get("/", (req, res) => res.send("Hello world"));
 app.get("/products", (req, res) => res.json(data.products));
+app.get("/fashions", (req, res) => res.json(data.fashions));
+app.get("/households", (req, res) => res.json(data.households));
+app.get("/phones", (req, res) => res.json(data.phones));
+app.get("/computers", (req, res) => res.json(data.computers));
+app.get("/electronics", (req, res) => res.json(data.electronics));
 app.listen(port);
  
 console.log("api running on" + port + ":");
